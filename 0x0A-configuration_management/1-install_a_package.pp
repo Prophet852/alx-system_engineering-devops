f8d8dcb37cc091f7905376d7abecbbaae049deb8
Prophet852 <dingawillie@outlook.com>
2024-03-03 09:23:47 -0800

    config management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 # installs the package puppet-lint
 package { 'puppet-lint':
   ensure   => '2.1.1',
   provider => 'gem',
-=======
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
->>>>>>> c307f6def84179856605542a3f9aa85b05f81af4
 }
