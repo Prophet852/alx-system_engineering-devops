73929eb48ca1870eaa5c676ed23a590640c2f43b
Prophet852 <56918019+Prophet852@users.noreply.github.com>
2024-06-05 13:41:27 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,16 @@ Function to query number of subcribers on a given reddit subreddit """
 
 import requests
 
-def get_subreddit_subscribers(subreddit):
+def number_of_subcribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'user-agent': 'request'}
+    response = request.get(url, heads=headers, allow_redirects=false)
     
-    try:
-        response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).json()
-        subscribers = response['data']['subscribers']
-        return subscribers
-    except:
+    If response .status_code !=200:
         return 0
-
+        
+    data = response.json().get("data)
+    num_subs = data.get("subcribers")
+    
+    return num_subs
+    
