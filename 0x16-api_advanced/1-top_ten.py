5221622cd4931c16f1be4f6aa09e7dce3d14568d
Prophet852 <56918019+Prophet852@users.noreply.github.com>
2024-06-05 19:15:30 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,8 +7,7 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "linux:0x16-api_advanced:v1.0.0 (by /u/prophet852)"
     }
     params = {
         "limit": 10
